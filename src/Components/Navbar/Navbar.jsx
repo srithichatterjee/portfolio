e30e002d0d87fbb6eb0d038b483f0a288d27833c
Srithi <srithichatterjee@gmail.com>
2025-07-31 11:31:53 +0530

    Added react-anchor-link-smooth-scroll and updated Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import underline from '../../assets/nav_underline.svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import menu_open from '../../assets/menu_open.svg';
 import menu_close from '../../assets/menu_close.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
   const menuRef = useRef();
